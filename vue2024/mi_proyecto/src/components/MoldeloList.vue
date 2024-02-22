21e6544a62e06625f62adc28efb82c55eb53dfd6
petrix12 <bazo.pedro@gmail.com>
2024-02-22 16:28:40 +0100

    vue - servicios

---
+++ vue2024/mi_proyecto/src/components/MoldeloList.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h2>Lista de modelos</h2>
+        <ul>
+            <li v-if="modelo in modelos" :key="modelo.id">{{ modelo.name }}</li>
+        </ul>
+    </div>
+</template>
+
+<!-- Composición API -->
+<script>
+import { defineComponent, onMounted } from 'vue'
+import ModeloService from '@/services/ModeloService'
+
+export default defineComponent({
+    name: 'ModeloList',
+    setup() {
+        const service = new ModeloService()
+        const modelos = service.getModelos()
+
+        onMounted(async () => {
+            await service.fetchAll()
+        })
+
+        return {
+            modelos
+        }
+    },
+})
+</script>
+
+<style scoped>
+</style>
